2af5247fd36774b06926695bd9acd094c75c2ded
alexlittle <alex@digital-campus.org>
2016-04-18 13:53:56 +0300

    update ignore

--- settings.php
+++ settings.php
@@ -8,9 +8,15 @@ $DB->DBPASS = "password"; // moodle database password
 
 $ADAPT = new stdClass();
 $ADAPT->COURSES = array(1,2,3); // array of the Moodle course ids to apply the scripts to
-$ADAPT->VIEW_EXT_LINK = "http://<my.moodle.url>/mod/page/view.php?id="; // url to your Moodle site
-$ADAPT->EDIT_EXT_LINK = "http://<my.moodle.url>/course/modedit.php?return=0&sr=0&update="; // url to your Moodle site
+$ADAPT->VIEW_EXT_LINK = "http://<my.moodle.url>/mod/page/view.php?id=%d"; // url to your Moodle site
+$ADAPT->EDIT_EXT_LINK = "http://<my.moodle.url>/course/modedit.php?return=0&sr=0&update=%d"; // url to your Moodle site
 
 // specify output directory
+$OUTPUT_DIR = dirname(__FILE__)."/_output/";
+
+// create output dir if is doesn't exist
+if (!is_dir($OUTPUT_DIR)){
+	mkdir($OUTPUT_DIR);
+}
 
 
